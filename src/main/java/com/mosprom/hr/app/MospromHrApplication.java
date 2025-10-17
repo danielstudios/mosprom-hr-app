711062c680111362b1ded5394bec00edff0e993f
Irem <irem.ashfak@gmail.com>
2025-10-17 18:59:55 +0300

    инициализация

--- src/main/java/com/mosprom/hr/app/MospromHrApplication.java
+++ src/main/java/com/mosprom/hr/app/MospromHrApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MospromHrApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MospromHrApplication.class, args);
     }
